6cb2aa9135d0086f9ca0f2f741c3bcc8aa0a53c8
SpursFan21 <duncanfrederic@gmail.com>
2025-06-08 20:27:42 +1200

    ticket bug fixed

--- user-service/routes/user_routes.go
+++ user-service/routes/user_routes.go
@@ -9,23 +9,18 @@ import (
 )
 
 func SetupUserRoutes(app *fiber.App) {
-	// Everything is imposed with global middleware in main
 	userGroup := app.Group("/users")
 
-	// User profile routes
-	userGroup.Get("/:id", handlers.GetUser)
-	userGroup.Put("/:id", handlers.UpdateUser)
-	userGroup.Put("/:id/password", handlers.UpdatePassword)
-
-	// Job and order routes
-	userGroup.Get("/me/completed-jobs", handlers.GetCompletedJobs)
-
-	// Unified order details route
-	userGroup.Get("/order-details/:orderId", handlers.GetFullOrderDetails)
-
-	// ticket
+	// Static routes
 	userGroup.Get("/tickets", handlers.GetMyTickets)
 	userGroup.Post("/tickets", handlers.SubmitTicket)
 	userGroup.Post("/tickets/:id/reply", handlers.ReplyToTicket)
 
+	userGroup.Get("/me/completed-jobs", handlers.GetCompletedJobs)
+	userGroup.Get("/order-details/:orderId", handlers.GetFullOrderDetails)
+
+	// Dynamic routes
+	userGroup.Get("/:id", handlers.GetUser)
+	userGroup.Put("/:id", handlers.UpdateUser)
+	userGroup.Put("/:id/password", handlers.UpdatePassword)
 }
